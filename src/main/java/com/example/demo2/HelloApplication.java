54d67e41584c8b2ccefa436293d943bef2aa7db1
Swapnaroop <swapnaroop2001@gmail.com>
2023-12-10 20:49:59 -0500

    initialize

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -1,8 +1,12 @@
 package com.example.demo2;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -19,15 +23,12 @@ public class HelloApplication extends Application{
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("newdemo2fxml.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 850, 620);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 620);
+        Group root=new Group();
+
+
+        stage.setTitle("Apple Crusher");
         stage.setScene(scene);
-
-        // Add your other JavaFX nodes to the root VBox
-
-
-
-
 
         stage.show();
     }
--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -15,6 +15,17 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 
 public class HelloController {
+    public Label label3;
+    Double appleSpeed;
+
+    public void initialize() {
+        double randomNumber = 100*Math.random();
+        randomNumber=Math.round(randomNumber * 100.0) / 100.0;
+        String strNumber1 = String.valueOf(randomNumber);
+        label3.setText(strNumber1);
+        appleSpeed=randomNumber;
+        label3.setText(strNumber1); // Set the text for the label
+    }
     @FXML
     public Pane Apane;
     @FXML
@@ -32,34 +43,27 @@ public class HelloController {
     double initialYVelocity;
     double timeOfFlight;
 
-    public void Input(){
+
+
+    Double posX;
+    Double posY;
+
+
+    private static final double GRAVITY = 9.81; // Acceleration due to gravity (m/s^2)
+    @FXML
+
+
+
+    public void GenerateAppleSpeed(){
+
+    }
+
+    public void motion(){
         initialVelocity=Double.parseDouble(VelocityBox.getText());
         angle=Math.toRadians(Double.parseDouble(AngleBox.getText()));
         initialXVelocity = initialVelocity * Math.cos(angle)*1.4;
         initialYVelocity = initialVelocity * Math.sin(angle)*1.4;
         timeOfFlight = (2 * initialVelocity) / GRAVITY;
-    }
-
-    Double posX;
-    Double posY;
-
-    Double appleSpeed;
-    //public double INITIAL_SPEED = 20; // Initial horizontal speed (m/s)
-    //double horizontalDistance = INITIAL_SPEED * timeOfFlight;
-    private static final double GRAVITY = 9.81; // Acceleration due to gravity (m/s^2)
-    @FXML
-    public Label label3;
-
-
-    public void GenerateAppleSpeed(){
-        double randomNumber = 100*Math.random();
-        randomNumber=Math.round(randomNumber * 100.0) / 100.0;
-        String strNumber1 = String.valueOf(randomNumber);
-        label3.setText(strNumber1);
-        appleSpeed=randomNumber;
-    }
-
-    public void motion(){
 
         new AnimationTimer() {
             double time = 0;
@@ -72,20 +76,11 @@ public class HelloController {
                 Circle dot=new Circle(posX+800, posY+602, 1, Color.BLACK);
                 Apane.getChildren().add(dot);
                 time += 0.01;
-
                 double n=804+Arrow.getCenterX();
                 double m=ball.getCenterX();
                 double k=388-ball.getCenterY();
                 double l=-Arrow.getCenterY();
-
-                //System.out.println(n+",   "+m + "n-m = "+(n-m)/*+" "+"Y:"+" "+Math.abs(Arrow.getCenterY())+","+ball.getCenterY()*/);
-                System.out.println(k+":   "+l+":   k-l="+(l-k));
-                //double q=Math.abs((ball.getCenterX()-(804+Arrow.getCenterX())));
-                //double p=Math.abs(((ball.getCenterY()-(-Arrow.getCenterY()))));
-                //if (q<=30 && p<=30) {
-                    //stop();
-                //};
-                if(Math.abs(n-m)<=30 && Math.abs(k-l)<=20){
+                if(Math.abs(n-m)<=10 && Math.abs(k-l)<=10){
                     stop();
                 }
                 if (posY >=9) {
@@ -99,7 +94,7 @@ public class HelloController {
                     posY = Double.valueOf(455);
                     time = 0.0;
                 }
-                time += 0.05; // Increment time for the next frame
+                time += 0.05;
             }
         }.start();
         new AnimationTimer() {
@@ -114,14 +109,11 @@ public class HelloController {
                 Circle dot=new Circle(x+30, y+225, 1, Color.BLACK);
                 Apane.getChildren().add(dot);
                 time += 0.05;
-                //double q=Math.abs((ball.getCenterX()-(804+Arrow.getCenterX())));
-                //double p=Math.abs(((ball.getCenterY()-Arrow.getCenterY())));
-
                 double n=804+Arrow.getCenterX();
                 double m=ball.getCenterX();
                 double k=388-ball.getCenterY();
                 double l=-Arrow.getCenterY();
-                if(Math.abs(n-m)<=30 && Math.abs(k-l)<=20){
+                if(Math.abs(n-m)<=10 && Math.abs(k-l)<=10){
                     stop();
                 }
                 if (y >=387 ) {
